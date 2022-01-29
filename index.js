bb716dc4f0fe1c73a28114b41c06cd8138df3d2d
Miguel Angel Abad <miguel.abad86@gmail.com>
2022-01-29 18:51:46 +0100

    Complete Exercise 3.1-3.6

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const app = express()
+const persons = require('./data.json')
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (req, res) => {
+  res.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+    `)
+})
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
+app.post('/api/persons', (req, res) => {
+  const { body } = req
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'content missing',
+    })
+  }
+
+  const person = {
+    id: Date.now(),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons.find((person) => {
+    if (person.name === body.name) {
+      return res.status(401).json({
+        error: 'name must be unique',
+      })
+    }
+  })
+  persons.push(person)
+  res.status(201).json(person)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find((person) => person.id === id)
+  person
+    ? res.status(200).json(person)
+    : res.status(404).json({
+        error: 'person not found',
+      })
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons.splice(id, 1)
+  res.status(204).end()
+})
+
+const PORT = 5005
+app.listen(PORT, () => {
+  console.log(`Server 🏃 on port ${PORT}`)
+})
